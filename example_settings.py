e38eec4f4e3d89a9939ef5b7f4a3cf24ddfe8eaa
Andy Byers <andy.byers@ubiquitypress.com>
2016-05-17 09:29:00 +0100

    working prototype

--- example_settings.py
+++ example_settings.py
@@ -8,3 +8,5 @@ DB_NAME = ""
 # a list of email addresses, if an author's email matches one in the
 # list it will be excluded from the results
 EXCLUDED_EMAILS = ['test@example.com']
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+import datetime
+import csv
+import os
+
 import db
 import settings
 from pprint import pprint
@@ -16,7 +20,7 @@ def build_csv_row(doi, author):
 	else:
 		orcid = ''
 
-	record = [doi, name, author.get('email'), orcid]
+	return [doi, name, author.get('email'), orcid]
 
 def get_csv_data(cursor, published_articles, journal_code):
 
@@ -37,6 +41,25 @@ def get_csv_data(cursor, published_articles, journal_code):
 					row = build_csv_row(doi, author)
 					records.append(row)
 
+	return records
+
+def generate_csv(journal, records):
+
+	date = datetime.date.today()
+
+	year = date.year
+	month = date.month
+	day = date.day
+
+	file_name = "{0}-doi-{1}-{2}-{3}.csv".format(journal.get('path'), year, month, day)
+
+	pprint(records)
+	with open(os.path.join(settings.BASE_DIR, 'csv', file_name), 'w') as fp:
+	    a = csv.writer(fp, delimiter=',')
+	    for row in records:
+	    	print row
+	    	a.writerow(row)
+
 # connect to the database
 cursor, con = db.connect_to_db()
 
@@ -44,11 +67,17 @@ cursor, con = db.connect_to_db()
 journals = db.multi_getter_where(cursor, table='journals', where='enabled=1')
 
 for journal in journals:
-	print journal.get('path')
+	print "Generating CSV for {0}".format(journal.get('path'))
 
 	# grab this journal's published articles
 	published_articles = db.get_published_articles(cursor, journal.get('journal_id'))
-	get_csv_data(cursor, published_articles, journal.get('path'))
+
+	# get csv record rows for each author
+	records = get_csv_data(cursor, published_articles, journal.get('path'))
+	print "{0} records found".format(len(records))
+
+	# export a csv for these records
+	generate_csv(journal, records)
 
 
 db.close(con)
